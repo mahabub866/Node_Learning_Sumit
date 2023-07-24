8e3bc75181e78f391f37c19e6cefe7715008c5f6
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2023-07-24 17:33:18 +0600

    ss

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const _ = require('loadsh');
+const _ = require("loadsh");
 
-console.log('dasdfs')
-    console.log('dasdfzcass');
-   console.log(_.last([1,2,3]));
+console.log("dasdfs");
+console.log("dasdfzcass");
+console.log(_.last([1, 2, 3]));
+
+
+setTimeout(()=>{
+    console.log('mak')
+},1000)
