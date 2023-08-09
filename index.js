da98a57907aee3f181c7002326bcc826afadc715
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2023-08-09 17:09:59 +0600

    import export moduler

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const _ = require("loadsh");
+const people=require('./people')
 
-console.log("dasdfs");
-console.log("dasdfzcass");
-console.log(_.last([1, 2, 3]));
+console.log(people)
 
+// console.log("dasdfs");
+// console.log("dasdfzcass");
+// console.log(_.last([1, 2, 3]));
+// console.log(__dirname)
+// console.log(__filename)
 
 setTimeout(()=>{
     console.log('mak')
---
+++ people.js
@@ -0,0 +1,7 @@
+const people=['maahbub','ucchash','namzmul'];
+
+
+var a=6
+
+
+module.exports=people
