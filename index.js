69f9b3b7bdb7cb818f449b854d91baa16db90f8c
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2023-08-10 12:41:24 +0600

    path module

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const _ = require("loadsh");
-const people=require('./people')
+const path= require('path')
+const mypath="/mak/jak/tak/index.js"
 
-console.log(people)
-console.log(_.last(people.people))
-
-// console.log("dasdfs");
-// console.log("dasdfzcass");
-// console.log(_.last([1, 2, 3]));
-// console.log(__dirname)
-// console.log(__filename)
-
-setTimeout(()=>{
-    console.log('mak')
-},1000)
+console.log(path.parse(mypath))
