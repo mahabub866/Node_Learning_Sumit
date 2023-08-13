41cdc05e5baf26fa275baa84fd09f0e0c55df533
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2023-08-13 18:14:59 +0600

    read straeming done

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-const fs=require('fs');
+const http=require('http')
 
-const ourReadStraem=fs.createReadStream(`${__dirname}/bigdata.txt`,'utf8')
+const server=http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.write("helo mahabub");
+        res.write(" helo mahi");
+        res.end();
+    }
+    else if (req.url==='/about'){
 
-ourReadStraem.on('data',(chunk)=>{
-    console.log(chunk)
-})
+        res.write("helo about mahabub");
+        res.end();
+    }
+    else{
+        res.write("Not found");
+        res.end();
+    }
+
+
+
+
+
+});
+
+
+// server.on('connection',(socket)=>{
+//    console.log('new connect')
+// })
+server.listen(3000);
+
+console.log('Liseting on port')
+
+
+
+
+ 
+
+
+
+// const fs=require('fs');
+
+// const ourReadStraem=fs.createReadStream(`${__dirname}/bigdata.txt`,'utf8')
+
+// ourReadStraem.on('data',(chunk)=>{
+//     console.log(chunk)
+// })
 
 
 
