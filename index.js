774bebcdeb751fbb7bf201f5c81f14c5bbdb0ad1
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2023-08-17 17:50:44 +0600

    static file read

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const express=require('express')
 
 const app = express()
 
-app.use(express.json())
+app.use(express.static(`${__dirname}`+'/public/'))
 
 app.get('/',(req,res)=>{
    res.send('This is home pge')
 })
 app.post('/',(req,res)=>{
 
- console.log(req.body)
    res.send('This is home pge with post req')
 })
 
