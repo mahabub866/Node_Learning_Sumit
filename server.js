025ebd8f0285cde94d67e9c24bfd2f10bf21d386
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2024-01-20 18:07:01 +0600

    1.33.37 miniute done..

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+ const http=require('http')
+ const fs=require('fs')
+ const fsPromises=require('fs').promises
+ const path=require('path')
+
+
  const logEvents=require('./logEvents')
  const EventEmitter=require('events')
- class MyEmitter extends EventEmitter {};
+ class Emitter extends EventEmitter {};
+ 
+//  initial object
+ const myEmitter=new Emitter();
+ 
+ const PORT= process.env.PORT || 3500;
 
- const myEmitter=new MyEmitter();
+ const server = http.createServer((req,res)=>{
+    console.log(req.url,req.method)
+    
+ })
+
+ server.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)});
 
 
  //add listerner
- myEmitter.on('log',(msg)=>logEvents(msg));
+//  myEmitter.on('log',(msg)=>logEvents(msg));
 
- setTimeout(()=>{
-    myEmitter.emit('log','log event emitted!')
- },2000);
+//  setTimeout(()=>{
+//     myEmitter.emit('log','log event emitted!')
+//  },2000);
